e8677a241053f72124be8f3138bcdc1e3f3f42d7
GET Academy <terje@kolderup.net>
2022-09-25 13:21:13 +0200

    x

--- js/game.js
+++ js/game.js
@@ -1,3 +1,15 @@
 function runGame() {
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+    const gameModel = initGameModel();
+    window.addEventListener('keydown', e => handleKey(true, e.key, gameModel));
+    window.addEventListener('keyup', e => handleKey(false, e.key, gameModel));
+}
 
+function initGameModel() {
+    return {
+        keys: new Set(),
+    };
 }
---
+++ js/inputHandler.js
@@ -0,0 +1,7 @@
+function handleKey(isKeyUp, key, gameModel){
+    if(!key.startsWith('Arrow'))return;
+    const keys = gameModel.keys;
+    if(isKeyUp) keys.add(key);
+    else keys.delete(key);
+    console.log(isKeyUp, key, gameModel.keys)
+}
