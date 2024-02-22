1cebfa550a212a40591c61de25c80c2be723666f
GET Academy <terje@getacademy.no>
2024-02-22 13:30:56 +0100

    Update game.js

--- functional/js/game.js
+++ functional/js/game.js
@@ -5,26 +5,26 @@ const init = () => {
     canvas.height = 720;
     const initialState = {
         x: 0,
-        speed: -2,        
+        speed: -2,
     };
-    const images = ['background','player','enemy']
-    .reduce((obj,name)=>R.assoc(name, getImage(name), obj), {});
-    console.log(images);
+    const getImage = name => document.getElementById(name + 'Img');
+    const images = ['background', 'player', 'enemy']
+        .reduce((obj, name) => R.assoc(name, getImage(name), obj), {});
+    const updateBackground = (state, imageWidth) => ({ ...state, x: (state.x + state.speed) % imageWidth });
 
-    const gameLoop = R.curryN(4,(ctx, images, state) => {
+    const drawBackground = R.curry((ctx, image, x) => {
+        ctx.drawImage(image, x, 0);
+        ctx.drawImage(image, x + image.width - 1, 0);
+    })(ctx, images.background);
+
+
+
+    const gameLoop = R.curryN(4, (ctx, images, state) => {
         const newState = updateBackground(state, images.background.width);
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        drawBackground(ctx, images.background, newState.x);
+        drawBackground(newState.x);
         requestAnimationFrame(gameLoop(newState));
     })(ctx, images);
     gameLoop(initialState, null);
 }
 
-const updateBackground = (state,imageWidth) => ({ ...state, x: (state.x + state.speed) % imageWidth });
-
-const drawBackground = (ctx, image, x) => {
-    ctx.drawImage(image, x, 0);
-    ctx.drawImage(image, x + image.width - 1, 0);
-};
-
-const getImage = name => document.getElementById(name + 'Img');
