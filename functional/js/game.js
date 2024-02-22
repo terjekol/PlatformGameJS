03298e5cc4209f7a7312d77b6009da9a521c3c6c
GET Academy <terje@getacademy.no>
2024-02-22 14:21:37 +0100

    Update game.js

--- functional/js/game.js
+++ functional/js/game.js
@@ -4,8 +4,13 @@ const init = () => {
     canvas.width = 800;
     canvas.height = 720;
     const initialState = {
-        x: 0,
-        speed: -2,
+        background: {
+            x: 0,
+            speed: -2,
+        }, 
+        player: {
+            x: 0,
+        },
     };
     const getImage = name => document.getElementById(name + 'Img');
     const images = ['background', 'player', 'enemy']
@@ -13,7 +18,7 @@ const init = () => {
     const drawImage = R.curry(ctx.drawImage.bind(ctx));
     const drawSprite = R.curry((image, width, height, x, y, spriteIndex, playerMode) => {
         const frameY = playerMode * height;
-        const frameX = spriteIndex * width;    
+        const frameX = spriteIndex * width;
         ctx.drawImage(image, frameX, frameY,
             width, height, x, y,
             width, height);
@@ -23,13 +28,17 @@ const init = () => {
     const playerY = canvas.height - playerSpriteSize;
     const drawPlayer = drawSprite(images.player, playerSpriteSize, playerSpriteSize, R.__, playerY, R.__, R.__);
     const draw = state => {
-        drawBackground(state.x);
-        drawBackground(state.x + images.background.width - 1);
-        drawPlayer(0, 0, 0);
+        drawBackground(state.background.x);
+        drawBackground(state.background.x + images.background.width - 1);
+        drawPlayer(state.player.x, 0, 0);
     };
     const updateBackground = (state, imageWidth) => ({ ...state, x: (state.x + state.speed) % imageWidth });
     const gameLoop = R.curryN(4, (ctx, images, state) => {
-        const newState = updateBackground(state, images.background.width);
+        const newBackgroundState = updateBackground(state.background, images.background.width);
+        const newState = {
+            player: {...state.player},
+            background: newBackgroundState,
+        }
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         draw(newState);
         requestAnimationFrame(gameLoop(newState));
